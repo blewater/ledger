bd9b4f8b022ac9c0e944a887b79ca1a3cba06196
Mario Karagiorgas <salem8@gmail.com>
2021-03-22 20:57:49 +0200

    Sign trx with ledger device for stargate chain

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,18 @@
 #!/usr/bin/env node
 
-import { Secp256k1, Secp256k1Signature, sha256 } from '@cosmjs/crypto';
-import { HdPath, Slip10RawIndex, stringToPath } from '@cosmjs/crypto';
-import { fromBase64 } from '@cosmjs/encoding';
-import {
-  coins,
-  GasPrice,
-  makeCosmoshubPath,
-  makeSignDoc,
-  Msg,
-  Secp256k1HdWallet,
-  serializeSignDoc,
-  SigningCosmosClient,
-  StdFee,
-} from '@cosmjs/launchpad';
+import { stringToPath } from '@cosmjs/crypto';
+import { GasPrice, Secp256k1HdWallet } from '@cosmjs/launchpad';
 import { LedgerSigner } from '@cosmjs/ledger-amino';
 import {
   assertIsBroadcastTxSuccess,
   SigningStargateClient,
   SigningStargateClientOptions,
 } from '@cosmjs/stargate';
-import { sleep } from '@cosmjs/utils';
-import Transport from '@ledgerhq/hw-transport';
-import TransportNodeHid from '@ledgerhq/hw-transport-node-hid';
+import TransportNodeHidNE from '@ledgerhq/hw-transport-node-hid-noevents';
+
+// ts hack for improper export of TransportNodeHid class as default
+import TransportModule from '../node_modules/@ledgerhq/hw-transport-node-hid/lib/TransportNodeHid.js';
+const transport = TransportModule.default;
 
 const rpcEndpoint = 'http://0.0.0.0:26657';
 const emoneyPrefix = 'emoney';
@@ -30,21 +20,13 @@ const emoneyPrefix = 'emoney';
 // A funded genesis address
 const mnemonic =
   'play witness auto coast domain win tiny dress glare bamboo rent mule delay exact arctic vacuum laptop hidden siren sudden six tired fragile penalty';
-const defaultChainId = 'localnet_reuse';
-const defaultFee: StdFee = {
-  amount: coins(100, 'ungm'),
-  gas: '250',
-};
-const defaultSequence = '0';
-const defaultAccountNumber = '42';
 const defaultLedgerAddress = 'emoney13j6rftqlqrwv0ejkst4y2grq4swvrx5ad7gg45';
-// const recipient = 'emoney15qdefkmwswysgg4qxgqpqr35k3m49pkxeyrqj5';
 const defaultGasPrice = GasPrice.fromString('1ungm');
 
 const hdPath = stringToPath("m/44'/118'/0'/0/0");
 
 async function main() {
-  await ledgerConn();
+  const ledgerSigner = await ledgerConn();
 
   const wallet = await Secp256k1HdWallet.fromMnemonic(
     mnemonic,
@@ -69,12 +51,14 @@ async function main() {
 
   const amountToLedgerAddr = {
     denom: 'ungm',
-    amount: '100',
+    amount: '85000',
   };
 
   const before = await client.getBalance(defaultLedgerAddress, 'ungm');
   if (before !== null) {
-    console.log(`balance before ${before.amount}${before.denom}`);
+    console.log(
+      `ledger starting balance before ${before.amount}${before.denom}`
+    );
   }
   const memo = 'Enjoy your NGM';
 
@@ -90,7 +74,9 @@ async function main() {
   assertIsBroadcastTxSuccess(result);
   const after = await client.getBalance(defaultLedgerAddress, 'ungm');
   if (after !== null) {
-    console.log(`balance after ${after.amount}${after.denom}`);
+    console.log(
+      `balance before ledger account transfer ${after.amount}${after.denom}`
+    );
   }
 
   /**********************************************
@@ -100,32 +86,51 @@ async function main() {
     denom: 'ungm',
     amount: '10',
   };
+
+  const ledgerSigningClient = await SigningStargateClient.connectWithSigner(
+    rpcEndpoint,
+    ledgerSigner,
+    signingOptions
+  );
+
+  // const legacyClient = new SigningCosmosClient(rpcEndpoint, firstAccount.address, ledgerSigner);
+  const resultFromLedger = await ledgerSigningClient.sendTokens(
+    defaultLedgerAddress,
+    firstAccount.address,
+    [amountFromLedgerAddr],
+    'From ledger account'
+  );
+  assertIsBroadcastTxSuccess(resultFromLedger);
+
+  const afterLedgerTrx = await client.getBalance(defaultLedgerAddress, 'ungm');
+  if (afterLedgerTrx !== null) {
+    console.log(
+      `balance before ledger account transfer ${afterLedgerTrx.amount}${afterLedgerTrx.denom}`
+    );
+  }
 }
 
-async function ledgerConn() {
-  // const transport = await TransportNodeHid.open("");
+async function ledgerConn(): Promise<LedgerSigner> {
+  const devs = TransportNodeHidNE.getDevices();
+  console.log(devs[0]);
+  console.log(devs[0].path);
   const interactiveTimeout = 120_000;
-  // const transportClass:any = await import("@ledgerhq/hw-transport-node-hid");
-  console.log('****************');
-  console.log(TransportNodeHid);
-  console.log('****************');
-  // const transport = await TransportNodeHid.open('');
-  const transport = await TransportNodeHid.create(
+
+  const ledgerTransp = await transport.create(
     interactiveTimeout,
     interactiveTimeout
   );
-  // const transport = await TransportNodeHid.create(
-  //   interactiveTimeout,
-  //   interactiveTimeout
-  // );
-  transport.setDebugMode(true);
-  const signer = new LedgerSigner(transport, {
-    testModeAllowed: true, // seems not used in the code base
+  const signer = new LedgerSigner(ledgerTransp, {
+    testModeAllowed: true,
     hdPaths: [hdPath],
+    prefix: emoneyPrefix,
   });
 
   const accounts = await signer.getAccounts();
-  console.log(`${accounts.length}, ${accounts[0]}`);
+  console.log('Ledger accounts:', accounts.length);
+  console.log(accounts[0]);
+
+  return signer;
 }
 
 main().then(
---
+++ src/lib/decs.d.ts
@@ -0,0 +1,3 @@
+declare module '@ledgerhq/hw-transport-node-hid-singleton';
+declare module '@ledgerhq/hw-transport-node-hid-noevents';
+declare module '@ledgerhq/hw-transport-node-hid-noevents';
